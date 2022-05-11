84895031d31a6f5110d11d93b50b00c7a80936e0
Michael Ruocco <michael.ruocco@digital.homeoffice.gov.uk>
2022-05-11 02:00:52 +0100

    tidies up realm and adds simplistic audience validation against token

---
+++ src/main/java/uk/co/mruoc/app/config/JwtAudienceValidator.java
@@ -0,0 +1,38 @@
+package uk.co.mruoc.app.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.oauth2.core.OAuth2Error;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidatorResult;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Slf4j
+public class JwtAudienceValidator implements OAuth2TokenValidator<Jwt> {
+
+    private final Collection<String> audiences;
+
+    public JwtAudienceValidator(String... audiences) {
+        this(Arrays.asList(audiences));
+    }
+
+    public OAuth2TokenValidatorResult validate(Jwt jwt) {
+        if (isValid(jwt.getAudience())) {
+            return OAuth2TokenValidatorResult.success();
+        }
+        return OAuth2TokenValidatorResult.failure(buildError());
+    }
+
+    private boolean isValid(List<String> jwtAudiences) {
+        return jwtAudiences.stream().anyMatch(audiences::contains);
+    }
+
+    private static OAuth2Error buildError() {
+        return new OAuth2Error("invalid_token", "The required audience is missing", null);
+    }
+}
--- src/main/java/uk/co/mruoc/app/config/SecurityConfig.java
+++ src/main/java/uk/co/mruoc/app/config/SecurityConfig.java
@@ -1,35 +1,68 @@
 package uk.co.mruoc.app.config;
 
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.jwt.JwtValidators;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationProvider;
 import org.springframework.security.oauth2.server.resource.authentication.JwtIssuerAuthenticationManagerResolver;
 
 @Configuration
-@EnableWebSecurity
-@Slf4j
+
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final String[] issuerUris;
+    private final String[] audiences;
 
-    public SecurityConfig(@Value("${spring.security.oauth2.resourceserver.jwt.issuer-uris}") String[] issuerUris) {
+    public SecurityConfig(@Value("${spring.security.oauth2.resourceserver.jwt.issuer-uris}") String[] issuerUris,
+                          @Value("${spring.security.oauth2.resourceserver.jwt.audiences}") String[] audiences) {
         this.issuerUris = issuerUris;
+        this.audiences = audiences;
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        log.info(
-                "configuring jwt issuer authentication manager resolver with issuer uris {}",
-                Arrays.toString(issuerUris));
-        http.authorizeRequests()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .oauth2ResourceServer()
-                .authenticationManagerResolver(new JwtIssuerAuthenticationManagerResolver(issuerUris));
+        http.authorizeRequests(auth -> auth.anyRequest().authenticated())
+                .oauth2ResourceServer().authenticationManagerResolver(buildResolver());
     }
+
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    private JwtIssuerAuthenticationManagerResolver buildResolver() {
+        Map<String, AuthenticationManager> managers = new HashMap<>();
+        for (String issuer : issuerUris) {
+            JwtDecoder decoder = toDecoder(issuer);
+            JwtAuthenticationProvider provider = new JwtAuthenticationProvider(decoder);
+            provider.setJwtAuthenticationConverter(new JwtAuthenticationConverter());
+            managers.put(issuer, provider::authenticate);
+        }
+        return new JwtIssuerAuthenticationManagerResolver(managers::get);
+    }
+
+    private NimbusJwtDecoder toDecoder(String issuerUri) {
+        NimbusJwtDecoder decoder = JwtDecoders.fromOidcIssuerLocation(issuerUri);
+        OAuth2TokenValidator<Jwt> audienceValidator = new JwtAudienceValidator(audiences);
+        OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuerUri);
+        OAuth2TokenValidator<Jwt> withAudience = new DelegatingOAuth2TokenValidator<>(withIssuer, audienceValidator);
+        decoder.setJwtValidator(withAudience);
+        return decoder;
+    }
+
 }
